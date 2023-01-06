3884998711495095c8f3d7a64313e698d4b8341a
Frank Yuan <yuan_zhi_fei@hotmail.com>
2023-01-06 20:26:23 +0800

    feat: init commit, only can run

---
+++ CaptureScreen/frmAdjustImage.Designer.cs
@@ -0,0 +1,181 @@
+﻿namespace CaptureScreen
+{
+    partial class frmAdjustImage
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmAdjustImage));
+            this.picCapturedImage = new System.Windows.Forms.PictureBox();
+            this.colorDialog = new System.Windows.Forms.ColorDialog();
+            this.picBackGround = new System.Windows.Forms.PictureBox();
+            this.btnColorPicker = new System.Windows.Forms.Button();
+            this.picWhite = new System.Windows.Forms.PictureBox();
+            this.picGreen = new System.Windows.Forms.PictureBox();
+            this.btnClearArea = new System.Windows.Forms.Button();
+            this.btnDrawRect = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.btnSaveToClip = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.picCapturedImage)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picBackGround)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picWhite)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picGreen)).BeginInit();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // picCapturedImage
+            // 
+            this.picCapturedImage.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.picCapturedImage.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.picCapturedImage.Location = new System.Drawing.Point(-1, 56);
+            this.picCapturedImage.Name = "picCapturedImage";
+            this.picCapturedImage.Size = new System.Drawing.Size(1514, 694);
+            this.picCapturedImage.TabIndex = 0;
+            this.picCapturedImage.TabStop = false;
+            this.picCapturedImage.MouseDown += new System.Windows.Forms.MouseEventHandler(this.picCapturedImage_MouseDown);
+            this.picCapturedImage.MouseMove += new System.Windows.Forms.MouseEventHandler(this.picCapturedImage_MouseMove);
+            // 
+            // picBackGround
+            // 
+            this.picBackGround.BackColor = System.Drawing.Color.White;
+            this.picBackGround.Location = new System.Drawing.Point(6, 15);
+            this.picBackGround.Name = "picBackGround";
+            this.picBackGround.Size = new System.Drawing.Size(23, 23);
+            this.picBackGround.TabIndex = 1;
+            this.picBackGround.TabStop = false;
+            // 
+            // btnColorPicker
+            // 
+            this.btnColorPicker.Image = ((System.Drawing.Image)(resources.GetObject("btnColorPicker.Image")));
+            this.btnColorPicker.Location = new System.Drawing.Point(35, 15);
+            this.btnColorPicker.Name = "btnColorPicker";
+            this.btnColorPicker.Size = new System.Drawing.Size(23, 23);
+            this.btnColorPicker.TabIndex = 2;
+            this.btnColorPicker.UseVisualStyleBackColor = true;
+            this.btnColorPicker.Click += new System.EventHandler(this.btnColorPicker_Click);
+            // 
+            // picWhite
+            // 
+            this.picWhite.BackColor = System.Drawing.Color.White;
+            this.picWhite.Location = new System.Drawing.Point(73, 15);
+            this.picWhite.Name = "picWhite";
+            this.picWhite.Size = new System.Drawing.Size(23, 23);
+            this.picWhite.TabIndex = 3;
+            this.picWhite.TabStop = false;
+            this.picWhite.Click += new System.EventHandler(this.picWhite_Click);
+            // 
+            // picGreen
+            // 
+            this.picGreen.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(219)))), ((int)(((byte)(238)))), ((int)(((byte)(221)))));
+            this.picGreen.Location = new System.Drawing.Point(102, 15);
+            this.picGreen.Name = "picGreen";
+            this.picGreen.Size = new System.Drawing.Size(23, 23);
+            this.picGreen.TabIndex = 4;
+            this.picGreen.TabStop = false;
+            this.picGreen.Click += new System.EventHandler(this.picGreen_Click);
+            // 
+            // btnClearArea
+            // 
+            this.btnClearArea.Image = ((System.Drawing.Image)(resources.GetObject("btnClearArea.Image")));
+            this.btnClearArea.Location = new System.Drawing.Point(7, 16);
+            this.btnClearArea.Name = "btnClearArea";
+            this.btnClearArea.Size = new System.Drawing.Size(23, 23);
+            this.btnClearArea.TabIndex = 5;
+            this.btnClearArea.UseVisualStyleBackColor = true;
+            // 
+            // btnDrawRect
+            // 
+            this.btnDrawRect.Image = ((System.Drawing.Image)(resources.GetObject("btnDrawRect.Image")));
+            this.btnDrawRect.Location = new System.Drawing.Point(191, 16);
+            this.btnDrawRect.Name = "btnDrawRect";
+            this.btnDrawRect.Size = new System.Drawing.Size(23, 23);
+            this.btnDrawRect.TabIndex = 6;
+            this.btnDrawRect.UseVisualStyleBackColor = true;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.picBackGround);
+            this.groupBox1.Controls.Add(this.btnColorPicker);
+            this.groupBox1.Controls.Add(this.picWhite);
+            this.groupBox1.Controls.Add(this.picGreen);
+            this.groupBox1.Location = new System.Drawing.Point(36, 1);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(134, 45);
+            this.groupBox1.TabIndex = 7;
+            this.groupBox1.TabStop = false;
+            // 
+            // btnSaveToClip
+            // 
+            this.btnSaveToClip.Image = ((System.Drawing.Image)(resources.GetObject("btnSaveToClip.Image")));
+            this.btnSaveToClip.Location = new System.Drawing.Point(241, 16);
+            this.btnSaveToClip.Name = "btnSaveToClip";
+            this.btnSaveToClip.Size = new System.Drawing.Size(23, 23);
+            this.btnSaveToClip.TabIndex = 8;
+            this.btnSaveToClip.UseVisualStyleBackColor = true;
+            this.btnSaveToClip.Click += new System.EventHandler(this.btnSaveToClip_Click);
+            // 
+            // frmAdjustImage
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1511, 746);
+            this.Controls.Add(this.btnSaveToClip);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.btnDrawRect);
+            this.Controls.Add(this.btnClearArea);
+            this.Controls.Add(this.picCapturedImage);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "frmAdjustImage";
+            this.Text = "Adjust Image";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.Load += new System.EventHandler(this.frmAdjustImage_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.picCapturedImage)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picBackGround)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picWhite)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picGreen)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private PictureBox picCapturedImage;
+        private ColorDialog colorDialog;
+        private PictureBox picBackGround;
+        private Button btnColorPicker;
+        private PictureBox picWhite;
+        private PictureBox picGreen;
+        private Button btnClearArea;
+        private Button btnDrawRect;
+        private GroupBox groupBox1;
+        private Button btnSaveToClip;
+    }
+}
---
+++ CaptureScreen/frmAdjustImage.cs
@@ -0,0 +1,132 @@
+﻿using System.Drawing.Drawing2D;
+
+namespace CaptureScreen
+{
+    public partial class frmAdjustImage : Form
+    {
+        //These variables control the mouse position
+        int selectX;
+        int selectY;
+        int selectWidth;
+        int selectHeight;
+        public Pen selectPen;
+        public Image currentImage;
+
+        //This variable control when you start the right click
+        bool start = false;
+
+        public frmAdjustImage()
+        {
+            InitializeComponent();
+        }
+
+        private void frmAdjustImage_Load(object sender, EventArgs e)
+        {
+            currentImage = Clipboard.GetImage();
+            picCapturedImage.Image = currentImage;
+            var lastBackground = Properties.Settings.Default;
+            picBackGround.BackColor = Color.FromArgb(
+                int.Parse(lastBackground.BackgroundColorR),
+                int.Parse(lastBackground.BackgroundColorG),
+                int.Parse(lastBackground.BackgroundColorB));
+        }
+
+        private void btnColorPicker_Click(object sender, EventArgs e)
+        {
+            var result = colorDialog.ShowDialog();
+            if (result == DialogResult.OK)
+            {
+                picBackGround.BackColor = colorDialog.Color;
+            }
+        }
+
+        private void picCapturedImage_MouseDown(object sender, MouseEventArgs e)
+        {
+            //validate when user right-click
+            if (!start)
+            {
+                if (e.Button == MouseButtons.Left)
+                {
+                    //starts coordinates for rectangle
+                    selectX = e.X;
+                    selectY = e.Y;
+                    selectPen = new Pen(Color.Red, 1)
+                    {
+                        DashStyle = DashStyle.Solid
+                    };
+                }
+                picCapturedImage.Refresh();
+                //start control variable for draw rectangle
+                start = true;
+            }
+            else
+            {
+                //validate if there is image
+                if (picCapturedImage.Image == null)
+                    return;
+                //same functionality when mouse is over
+                if (e.Button == MouseButtons.Left)
+                {
+                    picCapturedImage.Refresh();
+                    selectWidth = e.X - selectX;
+                    selectHeight = e.Y - selectY;
+                    var location = new PointF() { X = selectX, Y = selectY };
+                    var size = new SizeF() { Width = selectWidth, Height = selectHeight };
+                    Console.WriteLine($"{location} {size}");
+                    var rectFToFill = new RectangleF(location, size);
+                    Bitmap _img = new(currentImage);
+                    using Graphics g = Graphics.FromImage(_img);
+                    SolidBrush shadowBrush = new(picBackGround.BackColor);
+                    g.FillRectangles(shadowBrush, new RectangleF[] { rectFToFill });
+                    Clipboard.SetImage(_img);
+                    Application.Exit();
+                }
+                start = false;
+            }
+        }
+
+        private void picCapturedImage_MouseMove(object sender, MouseEventArgs e)
+        {
+            //validate if there is an image
+            if (picCapturedImage.Image == null)
+                return;
+            //validate if right-click was trigger
+            if (start)
+            {
+                picCapturedImage.Refresh();
+                //set corner square to mouse coordinates
+                selectWidth = e.X - selectX;
+                selectHeight = e.Y - selectY;
+                //draw dotted rectangle
+                picCapturedImage.CreateGraphics().DrawRectangle(selectPen,
+                          selectX, selectY, selectWidth, selectHeight);
+            }
+        }
+
+        private void picGreen_Click(object sender, EventArgs e)
+        {
+            picBackGround.BackColor = picGreen.BackColor;
+            SaveBackColor();
+        }
+
+        private void picWhite_Click(object sender, EventArgs e)
+        {
+            picBackGround.BackColor = picWhite.BackColor;
+            SaveBackColor();
+        }
+
+        private void SaveBackColor()
+        {
+            var lastBackground = Properties.Settings.Default;
+            lastBackground.BackgroundColorR = picBackGround.BackColor.R.ToString();
+            lastBackground.BackgroundColorG = picBackGround.BackColor.G.ToString();
+            lastBackground.BackgroundColorB = picBackGround.BackColor.B.ToString();
+            Properties.Settings.Default.Save();
+        }
+
+        private void btnSaveToClip_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
