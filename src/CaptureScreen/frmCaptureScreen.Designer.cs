23026a762186db1b56cf3f492e79fc9c3b53cbf0
Frederick Yuan <yuan_zhi_fei@hotmail.com>
2024-02-14 12:48:46 +0800

    fix: change icon

--- src/CaptureScreen/frmCaptureScreen.Designer.cs
+++ src/CaptureScreen/frmCaptureScreen.Designer.cs
@@ -29,101 +29,99 @@
         private void InitializeComponent()
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmCaptureScreen));
-            this.picCaptureScreen = new System.Windows.Forms.PictureBox();
-            this.btnScreen1 = new System.Windows.Forms.Button();
-            this.btnScreen2 = new System.Windows.Forms.Button();
-            this.btnExit = new System.Windows.Forms.Button();
-            ((System.ComponentModel.ISupportInitialize)(this.picCaptureScreen)).BeginInit();
-            this.SuspendLayout();
+            picCaptureScreen = new PictureBox();
+            btnScreen1 = new Button();
+            btnScreen2 = new Button();
+            btnExit = new Button();
+            ((System.ComponentModel.ISupportInitialize)picCaptureScreen).BeginInit();
+            SuspendLayout();
             // 
             // picCaptureScreen
             // 
-            this.picCaptureScreen.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
-            | System.Windows.Forms.AnchorStyles.Left) 
-            | System.Windows.Forms.AnchorStyles.Right)));
-            this.picCaptureScreen.BackColor = System.Drawing.SystemColors.Control;
-            this.picCaptureScreen.Location = new System.Drawing.Point(0, 3);
-            this.picCaptureScreen.Name = "picCaptureScreen";
-            this.picCaptureScreen.Size = new System.Drawing.Size(1585, 862);
-            this.picCaptureScreen.TabIndex = 0;
-            this.picCaptureScreen.TabStop = false;
-            this.picCaptureScreen.MouseDown += new System.Windows.Forms.MouseEventHandler(this.picCaptureScreen_MouseDown);
-            this.picCaptureScreen.MouseMove += new System.Windows.Forms.MouseEventHandler(this.picCaptureScreen_MouseMove);
-            this.picCaptureScreen.MouseUp += new System.Windows.Forms.MouseEventHandler(this.picCaptureScreen_MouseUp);
-            this.picCaptureScreen.PreviewKeyDown += new System.Windows.Forms.PreviewKeyDownEventHandler(this.picCaptureScreen_PreviewKeyDown);
+            picCaptureScreen.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
+            picCaptureScreen.BackColor = SystemColors.Control;
+            picCaptureScreen.Location = new Point(0, 3);
+            picCaptureScreen.Name = "picCaptureScreen";
+            picCaptureScreen.Size = new Size(1585, 862);
+            picCaptureScreen.TabIndex = 0;
+            picCaptureScreen.TabStop = false;
+            picCaptureScreen.MouseDown += picCaptureScreen_MouseDown;
+            picCaptureScreen.MouseMove += picCaptureScreen_MouseMove;
+            picCaptureScreen.MouseUp += picCaptureScreen_MouseUp;
+            picCaptureScreen.PreviewKeyDown += picCaptureScreen_PreviewKeyDown;
             // 
             // btnScreen1
             // 
-            this.btnScreen1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
-            this.btnScreen1.FlatAppearance.BorderSize = 0;
-            this.btnScreen1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.btnScreen1.Image = ((System.Drawing.Image)(resources.GetObject("btnScreen1.Image")));
-            this.btnScreen1.Location = new System.Drawing.Point(1373, 12);
-            this.btnScreen1.Name = "btnScreen1";
-            this.btnScreen1.Size = new System.Drawing.Size(48, 48);
-            this.btnScreen1.TabIndex = 15;
-            this.btnScreen1.TabStop = false;
-            this.btnScreen1.UseVisualStyleBackColor = true;
-            this.btnScreen1.Click += new System.EventHandler(this.btnScreen1_Click);
-            this.btnScreen1.MouseEnter += new System.EventHandler(this.btnControlButton_MouseEnter);
-            this.btnScreen1.MouseLeave += new System.EventHandler(this.btnControlButton_MouseLeave);
+            btnScreen1.Anchor = AnchorStyles.Top | AnchorStyles.Right;
+            btnScreen1.FlatAppearance.BorderSize = 0;
+            btnScreen1.FlatStyle = FlatStyle.Flat;
+            btnScreen1.Image = (Image)resources.GetObject("btnScreen1.Image");
+            btnScreen1.Location = new Point(1373, 12);
+            btnScreen1.Name = "btnScreen1";
+            btnScreen1.Size = new Size(48, 48);
+            btnScreen1.TabIndex = 15;
+            btnScreen1.TabStop = false;
+            btnScreen1.UseVisualStyleBackColor = true;
+            btnScreen1.Click += btnScreen1_Click;
+            btnScreen1.MouseEnter += btnControlButton_MouseEnter;
+            btnScreen1.MouseLeave += btnControlButton_MouseLeave;
             // 
             // btnScreen2
             // 
-            this.btnScreen2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
-            this.btnScreen2.FlatAppearance.BorderSize = 0;
-            this.btnScreen2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.btnScreen2.Image = ((System.Drawing.Image)(resources.GetObject("btnScreen2.Image")));
-            this.btnScreen2.Location = new System.Drawing.Point(1427, 12);
-            this.btnScreen2.Name = "btnScreen2";
-            this.btnScreen2.Size = new System.Drawing.Size(48, 48);
-            this.btnScreen2.TabIndex = 14;
-            this.btnScreen2.TabStop = false;
-            this.btnScreen2.UseVisualStyleBackColor = true;
-            this.btnScreen2.Click += new System.EventHandler(this.btnScreen2_Click);
-            this.btnScreen2.MouseEnter += new System.EventHandler(this.btnControlButton_MouseEnter);
-            this.btnScreen2.MouseLeave += new System.EventHandler(this.btnControlButton_MouseLeave);
+            btnScreen2.Anchor = AnchorStyles.Top | AnchorStyles.Right;
+            btnScreen2.FlatAppearance.BorderSize = 0;
+            btnScreen2.FlatStyle = FlatStyle.Flat;
+            btnScreen2.Image = (Image)resources.GetObject("btnScreen2.Image");
+            btnScreen2.Location = new Point(1427, 12);
+            btnScreen2.Name = "btnScreen2";
+            btnScreen2.Size = new Size(48, 48);
+            btnScreen2.TabIndex = 14;
+            btnScreen2.TabStop = false;
+            btnScreen2.UseVisualStyleBackColor = true;
+            btnScreen2.Click += btnScreen2_Click;
+            btnScreen2.MouseEnter += btnControlButton_MouseEnter;
+            btnScreen2.MouseLeave += btnControlButton_MouseLeave;
             // 
             // btnExit
             // 
-            this.btnExit.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
-            this.btnExit.FlatAppearance.BorderSize = 0;
-            this.btnExit.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.btnExit.Image = ((System.Drawing.Image)(resources.GetObject("btnExit.Image")));
-            this.btnExit.Location = new System.Drawing.Point(1526, 12);
-            this.btnExit.Name = "btnExit";
-            this.btnExit.Size = new System.Drawing.Size(48, 48);
-            this.btnExit.TabIndex = 102;
-            this.btnExit.UseVisualStyleBackColor = true;
-            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
-            this.btnExit.MouseEnter += new System.EventHandler(this.btnControlButton_MouseEnter);
-            this.btnExit.MouseLeave += new System.EventHandler(this.btnControlButton_MouseLeave);
+            btnExit.Anchor = AnchorStyles.Top | AnchorStyles.Right;
+            btnExit.FlatAppearance.BorderSize = 0;
+            btnExit.FlatStyle = FlatStyle.Flat;
+            btnExit.Image = (Image)resources.GetObject("btnExit.Image");
+            btnExit.Location = new Point(1526, 12);
+            btnExit.Name = "btnExit";
+            btnExit.Size = new Size(48, 48);
+            btnExit.TabIndex = 102;
+            btnExit.UseVisualStyleBackColor = true;
+            btnExit.Click += btnExit_Click;
+            btnExit.MouseEnter += btnControlButton_MouseEnter;
+            btnExit.MouseLeave += btnControlButton_MouseLeave;
             // 
             // frmCaptureScreen
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1586, 865);
-            this.Controls.Add(this.btnExit);
-            this.Controls.Add(this.btnScreen1);
-            this.Controls.Add(this.btnScreen2);
-            this.Controls.Add(this.picCaptureScreen);
-            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
-            this.KeyPreview = true;
-            this.MaximizeBox = false;
-            this.MinimizeBox = false;
-            this.Name = "frmCaptureScreen";
-            this.ShowIcon = false;
-            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
-            this.Text = "CaptureScreen";
-            this.TopMost = true;
-            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
-            this.Load += new System.EventHandler(this.frmCaptureScreen_Load);
-            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.frmCaptureScreen_KeyDown);
-            this.MouseUp += new System.Windows.Forms.MouseEventHandler(this.frmCaptureScreen_MouseUp);
-            ((System.ComponentModel.ISupportInitialize)(this.picCaptureScreen)).EndInit();
-            this.ResumeLayout(false);
-
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(1586, 865);
+            Controls.Add(btnExit);
+            Controls.Add(btnScreen1);
+            Controls.Add(btnScreen2);
+            Controls.Add(picCaptureScreen);
+            FormBorderStyle = FormBorderStyle.None;
+            Icon = (Icon)resources.GetObject("$this.Icon");
+            KeyPreview = true;
+            MaximizeBox = false;
+            MinimizeBox = false;
+            Name = "frmCaptureScreen";
+            ShowIcon = false;
+            StartPosition = FormStartPosition.CenterScreen;
+            Text = "CaptureScreen";
+            TopMost = true;
+            WindowState = FormWindowState.Maximized;
+            Load += frmCaptureScreen_Load;
+            KeyDown += frmCaptureScreen_KeyDown;
+            MouseUp += frmCaptureScreen_MouseUp;
+            ((System.ComponentModel.ISupportInitialize)picCaptureScreen).EndInit();
+            ResumeLayout(false);
         }
 
         #endregion
