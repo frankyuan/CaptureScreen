1e868bb32a7140f92026d307b6e398d2e038a269
Frederick Yuan <yuan_zhi_fei@hotmail.com>
2025-06-06 20:26:16 +0800

    fix: try to fix two monitors have different scale and dpi

--- src/CaptureScreen/Program.cs
+++ src/CaptureScreen/Program.cs
@@ -1,15 +1,21 @@
 using System.Diagnostics;
+using System.Runtime.InteropServices;
 
 namespace CaptureScreen
 {
     internal static class Program
     {
+        [DllImport("user32.dll")]
+        private static extern bool SetProcessDpiAwarenessContext(IntPtr dpiContext);
+
+        private static readonly IntPtr DPI_AWARENESS_CONTEXT_PER_MONITOR_AWARE_V2 = new(-4);
         /// <summary>
         ///  The main entry point for the application.
         /// </summary>
         [STAThread]
         static void Main()
         {
+            SetProcessDpiAwarenessContext(DPI_AWARENESS_CONTEXT_PER_MONITOR_AWARE_V2);
             // To customize application configuration such as set high DPI settings or default font,
             // see https://aka.ms/applicationconfiguration.
             ApplicationConfiguration.Initialize();
